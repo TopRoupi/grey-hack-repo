2fd1f0fa7c7552190e834bd6310c393d16ee8e1c
a <breno.nunesgalvao@protonmail.ch>
2021-05-27 16:10:38 -0300

    fixed login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
