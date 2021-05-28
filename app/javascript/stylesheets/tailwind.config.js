c17636919b2ebff1bc0996df1f4550fa8d5b4eb5
a <breno.nunesgalvao@protonmail.ch>
2021-05-28 02:39:53 -0300

    enable public assets

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  // purge: [
-  //   "./app/**/*.html.erb",
-  //   "./app/helpers/**/*.rb",
-  //   "./app/javascript/**/*.js",
-  //   "./app/javascript/**/*.vue",
-  // ],
+  purge: [
+    "./app/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/javascript/**/*.js",
+    "./app/javascript/**/*.vue",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
