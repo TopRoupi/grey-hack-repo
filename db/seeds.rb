9161f2b84defffad7a8b71e03f838679b20dbc03
a <breno.nunesgalvao@protonmail.ch>
2021-05-28 00:57:23 -0300

    post show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,7 +3,7 @@
 
 Category.create name: "Hacking", icon: "cpu"
 Category.create name: "Snippets", icon: "code"
-Category.create name: "Virus", icon: "bug"
+Category.create name: "Viruses", icon: "bug"
 Category.create name: "Tools", icon: "tools"
 Category.create name: "Utils", icon: "terminal"
 Category.create name: "Others", icon: "number"
