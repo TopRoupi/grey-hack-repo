00eb4f4123db17af7e74463071ec79033845897f
a <breno.nunesgalvao@protonmail.ch>
2021-06-03 09:41:33 -0300

    updated gems and switched to stimulus-reflex 3.5.0-pre1

--- app/reflexes/example_reflex.rb
+++
@@ -1,5 +0,0 @@
-class ExampleReflex < ApplicationReflex
-  def test
-    morph("#render_here", render(Posts::Card::Component.new(post: Post.first), layout: false))
-  end
-end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,4 +77,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_controller.default_url_options = {host: "localhost", port: 3000}
+  config.action_mailer.default_url_options = {host: "localhost", port: 3000}
 end
