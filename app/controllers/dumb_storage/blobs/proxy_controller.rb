98f2eea07a95d2fec02decc4bd02bcd80e959ccb
a <breno.nunesgalvao@protonmail.ch>
2021-06-04 10:13:15 -0300

    clean up code used for tests

--- app/controllers/dumb_storage/blobs/proxy_controller.rb
+++
@@ -1,7 +0,0 @@
-class DumbStorage::Blobs::ProxyController < ActiveStorage::BaseController
-  # include ActiveStorage::SetBlob
-
-  def show
-    puts "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
-  end
-end
--- app/controllers/dumb_storage/blobs/redirect_controller.rb
+++
@@ -1,7 +0,0 @@
-class DumbStorage::Blobs::RedirectController < ActiveStorage::BaseController
-  # include ActiveStorage::SetBlob
-
-  def show
-    puts "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
-  end
-end
