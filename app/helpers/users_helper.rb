2da0400d60786758ce20230aaf5ff8a7c8eca8d7
a <breno.nunesgalvao@protonmail.ch>
2021-06-10 13:08:57 -0300

    user avatar helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 module UsersHelper
+  def user_avatar(user)
+    user.avatar || default_avatar
+  end
+
+  def current_user_avatar
+    if current_user
+      user_avatar(current_user)
+    else
+      default_avatar
+    end
+  end
+
+  private
+
+  def default_avatar
+    "/assets/user.png"
+  end
 end
