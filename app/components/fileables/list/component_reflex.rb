bccd3d9c02c7bfc8d0fba782dc4f5e08f847259d
a <breno.nunesgalvao@protonmail.ch>
2021-12-08 14:25:55 -0300

    remove_script reflex

--- app/components/fileables/list/component_reflex.rb
+++ app/components/fileables/list/component_reflex.rb
@@ -7,8 +7,18 @@ class Fileables::List::ComponentReflex < ApplicationReflex
     @fileable = session[:forms]["Post"]
   end
 
-  def add_script
-    session[:forms]["Post"].scripts.build
+  after_reflex do
+    session[:forms]["Post"] = @fileable
     morph dom_id(@fileable, "fileable"), render(Fileables::List::Component.new(fileable: @fileable, edit: true))
+  end
+
+  def add_script
+    @fileable.scripts.build
+  end
+
+  def remove_script
+    index = element.dataset[:index].to_i
+    removed_script = @fileable.scripts[index]
+    @fileable.scripts.delete(removed_script)
   end
 end
