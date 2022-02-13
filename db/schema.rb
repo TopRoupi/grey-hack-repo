4c31f7fbeb0ea8012dce91b07a35c4acd55e839a
a <breno.nunesgalvao@protonmail.ch>
2022-02-13 02:07:17 -0300

    fix db script

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_02_07_015125) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_10_000439) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
     t.string "record_type", null: false
-    t.integer "record_id", null: false
+    t.bigint "record_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
@@ -50,6 +51,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_07_015125) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "builds", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_builds_on_post_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -107,15 +116,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_07_015125) do
     t.datetime "updated_at", null: false
     t.index ["read_at"], name: "index_notifications_on_read_at"
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
-  end
-
-  create_table "post_categories", force: :cascade do |t|
-    t.bigint "post_id", null: false
-    t.bigint "category_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_post_categories_on_category_id"
-    t.index ["post_id"], name: "index_post_categories_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -179,9 +179,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_02_07_015125) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "builds", "posts"
   add_foreign_key "comments", "users"
-  add_foreign_key "post_categories", "categories"
-  add_foreign_key "post_categories", "posts"
   add_foreign_key "posts", "categories"
   add_foreign_key "posts", "users"
   add_foreign_key "stars", "users"
--- db/scripts/copy_post_files_to_build.rb
+++ db/scripts/copy_post_files_to_build.rb
@@ -3,8 +3,8 @@ require "rubygems"
 Post.all.each do |post|
   build = Build.new name: "Main build", post: post
   post_scripts = Script.where(scriptable_type: "Post", scriptable_id: post.id)
-  post_folders = Folder.where(foldabel_type: "Post", foldable_id: post.id)
+  post_folders = Folder.where(foldable_type: "Post", foldable_id: post.id)
   build.scripts = post_scripts
-  build.folder = post_folders
+  build.folders = post_folders
   build.save
 end
