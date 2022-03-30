c873c65fccb47b24d77bb6eab181cb603ed01d29
a <breno.nunesgalvao@protonmail.ch>
2022-03-30 08:27:13 -0300

    better resposive posts box component

--- app/components/categories/card/component.rb
+++ app/components/categories/card/component.rb
@@ -2,13 +2,13 @@
 
 class Categories::Card::Component < ApplicationComponent
   def initialize(category:, tag: :a, small: false, active: false, style: nil, **sys_params)
-    @style, @active_style = style || ["bg-gradient-to-r from-beaver-800 to-beaver-850", "bg-beaver-850"]
+    @style, @active_style = style || ["bg-gradient-to-r from-beaver-800 to-beaver-850", "bg-beaver-850 outline outline-1 outline-beaver-700"]
     @small = small
     @sys_params = sys_params
     @tag = tag
     @sys_params[:data] ||= {}
     @sys_params[:data][:controller] = "categories--card--component"
-    @sys_params[:class] = "my-2 mx-2 #{active ? @active_style : @style} #{@small ? "!mx-0 py-2 justify-start md:pl-3" : "py-4 justify-center"} rounded-lg w-full flex flex-col hover:text-white text-beaver-300 md:flex-row items-center"
+    @sys_params[:class] = "my-2 mx-2 #{active ? @active_style : @style} #{@small ? "!mx-0 sm:!mx-2 md:!mx-0 py-2 justify-start pl-3 sm:pl-0 md:pl-3" : "py-4 justify-center"} rounded-lg w-full flex flex-row hover:text-white text-beaver-300 sm:flex-col md:flex-row items-center"
     @category = category
 
     if category == :all
