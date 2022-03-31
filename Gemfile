57b44a6300f07c02f49d6aa657bb8c1ba648f326
a <breno.nunesgalvao@protonmail.ch>
2022-03-31 08:22:31 -0300

    sentry setup

--- Gemfile
+++ Gemfile
@@ -66,3 +66,6 @@ end
 gem "tzinfo-data", platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "sidekiq", "~> 6.4"
+
+gem "sentry-ruby", "~> 5.2"
+gem "sentry-rails", "~> 5.2"
---
+++ config/initializers/sentry.rb
@@ -0,0 +1,20 @@
+Sentry.init do |config|
+  config.environment = Rails.env
+  config.dsn = Rails.application.credentials[:sentry_dsn]
+  config.breadcrumbs_logger = [:active_support_logger, :http_logger]
+  config.enabled_environments = %w[production]
+
+  filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
+  config.before_send = lambda do |event, hint|
+    filter.filter(event.to_hash)
+  end
+
+  # Set tracesSampleRate to 1.0 to capture 100%
+  # of transactions for performance monitoring.
+  # We recommend adjusting this value in production
+  config.traces_sample_rate = 1.0
+  # or
+  config.traces_sampler = lambda do |context|
+    true
+  end
+end
