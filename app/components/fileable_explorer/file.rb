8dd8c3cf6887219d5baea29ca7f608a9150c4de2
a <breno.nunesgalvao@protonmail.ch>
2022-04-07 10:26:45 -0300

    finished build explorer component added download button and export string modal and added script show route

--- app/components/fileable_explorer/file.rb
+++ app/components/fileable_explorer/file.rb
@@ -1,17 +1,18 @@
 # frozen_string_literal: true
 
 class FileableExplorer::File < ApplicationComponent
-  def initialize(file:, tag: :button, **sys_params)
+  def initialize(file:, tag: nil, **sys_params)
     @file = file
     @sys_params = sys_params
-    @tag = tag
 
     if script?
       @icon = "file"
       @icon_color = "beaver-200"
+      @tag ||= :a
     else # folder
       @icon = "file-directory-fill"
       @icon_color = "blue-400"
+      @tag ||= :button
       @sys_params[:data] = {}
       @sys_params[:data][:reflex] = "click->FileableExplorerReflex#open"
       @sys_params[:data][:fileable_id] = @file.id
@@ -19,6 +20,10 @@ class FileableExplorer::File < ApplicationComponent
     end
 
     @sys_params[:class] = "text-#{@icon_color} hover:bg-beaver-800 rounded flex flex-col justify-center items-center"
+  end
+
+  def before_render
+    @sys_params[:href] = script_path(@file) if script?
   end
 
   def script?
