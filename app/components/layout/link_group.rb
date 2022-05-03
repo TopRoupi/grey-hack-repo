cb137f5385f80b1c259282434a7bb31814fdd2cf
a <breno.nunesgalvao@protonmail.ch>
2022-05-03 08:48:06 -0300

    users show ui improvements

--- app/components/layout/link_group.rb
+++ app/components/layout/link_group.rb
@@ -1,25 +1,29 @@
 # frozen_string_literal: true
 
 class Layout::LinkGroup < ApplicationComponent
-  renders_many :links, ->(link: nil, name: nil, disabled: link.nil?, active: false, html_class: {}) do
-    bg = html_class[:bg] || "bg-beaver-800 hover:bg-beaver-700"
-    bg = html_class[:bg_disabled] || "bg-beaver-850" if disabled
-    bg = html_class[:bg_active] || "bg-beaver-600" if active
+  renders_many :links, lambda { |link: nil, name: nil, active: false, disabled: link.nil?, **kwargs, &block|
+    bg = @dark ? "bg-beaver-900 hover:bg-beaver-850 border border-beaver-800" : "bg-beaver-800 hover:bg-beaver-700"
+    bg = @dark ? "bg-beaver-900 border border-beaver-800 text-beaver-300" : "bg-beaver-850" if disabled
+    bg = @dark ? "bg-beaver-850 border border-beaver-800" : "bg-beaver-600" if active
 
     rounded = ""
     rounded += "rounded-#{@vertical ? "t" : "l"}-md" if links.length == 0
     rounded += "rounded-#{@vertical ? "b" : "r"}-md" if links.length == @links_count - 1
 
-    border = " border-#{@vertical ? "b" : "r"} border-beaver-900" unless @links_count == 1 || links.length == @links_count - 1
+    border = " border-#{@vertical ? "b" : "r"} border-beaver-900" unless @dark || @links_count == 1 || links.length == @links_count - 1
+    margin = @vetical ? "margin-bottom: 1px" : "margin-right: 1px" if @dark
 
-    klass = [bg, rounded, border, html_class[:container] || "py-3 w-full text-center justify-center font-medium flex items-center"].join " "
+    kwargs[:class] = [bg, rounded, border, "py-3 w-full text-center justify-center font-medium flex items-center"].join " "
+    kwargs[:style] = margin
+    kwargs[:href] = link
 
-    Layout::LinkGroup::Link.new(link: active || disabled ? nil : link, name: name, class: klass)
-  end
+    content_tag(:a, **kwargs) { name || block.call }
+  }
 
-  def initialize(links_count, direction = :horizontal)
+  def initialize(links_count, direction = :horizontal, dark: false)
     @links_count = links_count
     @vertical = true if direction == :vertical
     @horizontal = true if direction == :horizontal
+    @dark = dark
   end
 end
