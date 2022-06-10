29fe2f1d9535e133eead1f24c12ba4fa510889a3
a <breno.nunesgalvao@protonmail.ch>
2022-06-10 03:49:27 -0300

    initial components migration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -146,5 +146,17 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/typography'),
+    require('daisyui'),
   ],
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "black",
+  },
 }
