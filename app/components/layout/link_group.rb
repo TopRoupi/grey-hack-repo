d878b4a0453a43b202262f7b21ab0b1518624e9f
a <breno.nunesgalvao@protonmail.ch>
2022-06-28 16:54:36 -0300

    improved user show page

--- app/components/layout/link_group.rb
+++ app/components/layout/link_group.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
 class Layout::LinkGroup < ApplicationComponent
-  renders_many :links, lambda { |link: nil, name: nil, active: false, disabled: link.nil?, **kwargs, &block|
-    classes = "shrink btn btn-block btn-neutral "
-    classes += "btn-disabled" if disabled
-    classes += "btn-active" if active
+  renders_many :links, lambda { |link: nil, name: nil, active: false, disabled: link.nil?, custom: {}, **kwargs, &block|
+    if custom.blank?
+      classes = "shrink btn btn-block btn-neutral "
+      classes += "btn-disabled" if disabled
+      classes += "btn-active" if active
+    else
+      classes = custom[:class]
+      classes += custom[:disabled] if disabled && custom[:disabled]
+      classes += custom[:active] if active && custom[:active]
+    end
 
     kwargs[:class] = classes
     kwargs[:href] = link
 
     content_tag(:a, **kwargs) { name || block.call }
   }
+
+  def initialize(gap: 1)
+    @gap = gap
+  end
 end
