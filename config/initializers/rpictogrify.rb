0500b8bc90a34e9c4e935c4ea104b2f829c94854
a <breno.nunesgalvao@protonmail.ch>
2022-07-30 07:59:21 -0300

    update gems/js

--- config/initializers/rpictogrify.rb
+++ config/initializers/rpictogrify.rb
@@ -1,14 +1,5 @@
 Rpictogrify.configure do
-  # default theme, one of these themes: avataars_female, avataars_male, male_flat, monsters. default is :monsters
   self.theme = :monsters
-  # pictogram directory. default is 'public/system'
   self.base_path = "tmp/nfts"
-  self.themes_assets_path = Rails.root.join("app", "rpictogrify", "themes")
-end
-
-module Rpictogrify
-  module Themes
-    class Hats < Base
-    end
-  end
+  register_theme :hats, assets_path: "vendor/assets/rpictogrify/themes/hats"
 end
