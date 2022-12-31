33b4b54d44d65b8104db7bc5da0d2873da48577e
a <breno.nunesgalvao@protonmail.ch>
2022-12-31 11:30:09 -0300

    add encoder function similar to my lzw script in greyhack

---
+++ app/models/grey_parser/lzw.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+class GreyParser::Lzw
+  def self.compress(uncompressed)
+    dict_size = 256
+    dictionary = Hash[ Array.new(dict_size) {|i| [i.chr, i.chr]} ]
+
+    w = ""
+    result = []
+    uncompressed.chars.each do |c|
+      wc = w + c
+      if dictionary.has_key?(wc)
+        w = wc
+      else
+        result << dictionary[w]
+        # Add wc to the dictionary.
+        dictionary[wc] = dict_size
+        dict_size += 1
+        w = c
+      end
+    end
+
+    result << dictionary[w] unless w.empty?
+    result
+  end
+
+  def self.decompress(compressed)
+    dict_size = 256
+    dictionary = Hash[ Array.new(dict_size) {|i| [i.chr, i.chr]} ]
+
+    w = result = compressed.shift
+    compressed.each do |k|
+      if dictionary.has_key?(k)
+        entry = dictionary[k]
+      elsif k == dict_size
+        entry = w + w[0, 1]
+      else
+        raise "Bad compressed k: #{k}"
+      end
+      result += entry
+
+      dictionary[dict_size] = w + entry[0, 1]
+      dict_size += 1
+
+      w = entry
+    end
+    result
+  end
+end
