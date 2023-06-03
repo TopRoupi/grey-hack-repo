07f530edd9e6e689ca3b0da23ee3daf7bd171828
a <breno.nunesgalvao@protonmail.ch>
2023-06-03 15:23:17 -0300

    fix n+1 on post card component

--- app/components/posts/card.rb
+++ app/components/posts/card.rb
@@ -4,6 +4,7 @@ class Posts::Card < ApplicationComponent
   def initialize(post:, current_user:)
     @current_user = current_user
     @post = post
-    @build = @post.builds.published.last
+    # @build = @post.builds.published.last
+    @build = @post.builds.select(&:published).sort_by(&:created_at).last
   end
 end
