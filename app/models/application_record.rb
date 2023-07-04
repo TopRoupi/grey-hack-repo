5ab16d46607cb36bcb256a482ba6978fa77c8f5b
a <breno.nunesgalvao@protonmail.ch>
2023-07-04 20:33:13 -0300

    fix tests

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -6,4 +6,9 @@ class ApplicationRecord < ActiveRecord::Base
   def stripped_attributes
     attributes.reject { |key| key.to_s.match(/\bid|created_at|updated_at/) }
   end
+
+  #TODO: remove this after https://github.com/rails/rails/issues/48652 is fixed
+  def deep_dup
+    self
+  end
 end
