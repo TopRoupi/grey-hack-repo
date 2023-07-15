0b2e891eee3b15266a109466520182de3f5967be
a <breno.nunesgalvao@protonmail.ch>
2023-07-15 12:35:55 -0300

    gzip assets

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import tailwindcss from 'tailwindcss'
+// import viteCompression from 'vite-plugin-compression';
+import gzipPlugin from 'rollup-plugin-gzip'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    gzipPlugin()
   ],
   css: {
     postcss: {
