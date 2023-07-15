2d627a0dd03b8292c526cff079d05ff301c72894
a <breno.nunesgalvao@protonmail.ch>
2023-07-15 08:27:48 -0300

    use different redis server only for sidekiq and switch to redis cache

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 Sidekiq.configure_server do |config|
-  config.redis = {url: ENV.fetch("REDIS_URL", "redis://localhost:6379")}
+  config.redis = {url: ENV.fetch("REDIS_SIDEKIQ_URL", "redis://localhost:6379")}
 end
